71ac65a0044f2d025565a585332c59987b454a68
MinPyaeHein <minpyaehein.ucsdawei@gmail.com>
2024-03-21 01:19:56 +0700

    sfsfhsf

--- app/models/tran_investor.rb
+++ app/models/tran_investor.rb
@@ -1,5 +1,4 @@
 class TranInvestor< ApplicationRecord
     belongs_to :team_event, :dependent => :destroy
-    belongs_to :investor_matrix, :dependent => :destroy
     belongs_to :judge, :dependent => :destroy
 end
