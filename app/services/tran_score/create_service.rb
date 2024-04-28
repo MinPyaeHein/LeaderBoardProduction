6c849127793022bf841163e0c538085893d2e383
Min Pyae Hein <minpyaehein@Mins-Laptop.true.true>
2024-04-28 13:19:11 +0700

    sfjosfjsjof

--- app/services/tran_score/create_service.rb
+++ app/services/tran_score/create_service.rb
@@ -6,16 +6,17 @@
         @params = params
       end
       def create
-        team_event = TeamEvent.find_by(event_id: @params[:event_id], team_id: @params[:team_id]) || (raise "Team Event does not exist in the database")
-        judge = Judge.find_by(member_id: @params[:judge_id],event_id: @params[:event_id]) || (raise "Judge does not exist in the database")
-        @score_matrix=ScoreMatrix.find_by(id: @params[:score_matrix_id]) || (raise "Score Matrix does not exist in the database")
-        # return { errors: "Tran Score already exists in the database" } if TranScore.find_by(score_matrix_id: @score_matrix.id, judge_id: judge.id, team_event_id: team_event.id)
+        team_event = TeamEvent.find_by(event_id: @params[:event_id], team_id: @params[:team_id])
+        return { errors: "Team Event does not exist in the database" } unless team_event
 
-        
+        judge = Judge.find_by(member_id: @params[:judge_id], event_id: @params[:event_id])
+        return { errors: "Judge does not exist in this event" } unless judge
+
+
         tran_score= ::TranScore.create(
           score:  @params[:score],
           desc:  @params[:desc],
-          score_matrix_id:  @score_matrix.id,
+          score_matrix_id:  @params[:score_matrix_id],
           judge_id:  judge.id,
           team_event_id: team_event.id,
           event_id: @params[:event_id]
