7502510f7960458b60dd10c3b9ceb368ce0d8f78
Min Pyae Hein <minpyaehein@Mins-Laptop.true.true>
2024-08-07 23:50:44 +0700

    fix server error

--- app/jobs/send_password_email_job.rb
+++
@@ -1,6 +0,0 @@
-# class SendPasswordEmailJob < ApplicationJob
-#   queue_as :default
-#   def perform(email, password, username)
-#     MemberMailer.welcome_email(email, password, username)
-#   end
-# end
