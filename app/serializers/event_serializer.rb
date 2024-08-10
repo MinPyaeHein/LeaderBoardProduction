e2b8e067eefa24119d0ec162807abd7d6d505d8a
Min Pyae Hein <minpyaehein@Mins-Laptop.true.true>
2024-08-10 22:23:24 +0700

    add organizer in event sealizer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,10 +1,8 @@
 # app/serializers/event_serializer.rb
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :status, :organizer_name, :desc, :active,
+  attributes :id, :name, :status, :desc, :active,
    :event_type, :updated_at, :created_at, :start_date, :end_date, :start_time,
    :end_time, :all_day, :location, :organizer_id, :score_type_id
+  has_one :organizer
 
-  def organizer_name
-    object.organizer.name
-  end
 end
