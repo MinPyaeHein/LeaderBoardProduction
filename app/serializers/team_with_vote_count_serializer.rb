9d93fb57e5683523632467622fe37677910aef5e
Min Pyae Hein <minpyaehein536h@prestigein.com>
2024-08-21 19:44:06 +0700

    add voute count status filter

--- app/serializers/team_with_vote_count_serializer.rb
+++ app/serializers/team_with_vote_count_serializer.rb
@@ -1,9 +1,8 @@
-# app/serializers/team_with_vote_count_serializer.rb
 class TeamWithVoteCountSerializer < ActiveModel::Serializer
   attributes :id, :name, :status, :event_id, :organizer_id, :vote_count
   has_many :members
 
   def vote_count
-    object.member_votes.count
+    object.member_votes.where(status: true).count
   end
 end
