e8dff0078c5bb9d74aae3ac9eed6f5cf97da6047
Min Pyae Hein <minpyaehein536h@prestigein.com>
2024-08-22 19:03:18 +0700

    rule for create team member

--- app/services/judge/create_service.rb
+++ app/services/judge/create_service.rb
@@ -1,61 +1,64 @@
-# app/services/member_service.rb
+class Judge::CreateService
+  def initialize(params)
+    @params = params
+  end
 
+  def create
+    judges = []
+    errors = []
 
-    class Judge::CreateService
-      def initialize(params)
-        @params = params
-      end
-      def create
-        judges=[]
-        errors=[]
-       
-        @params[:member_ids].each do |member_id|
-          result=check_judge(member_id)
-        
-          if result[:errors].present?
-            puts "result_error: #{result[:errors]}"
-            errors << result[:errors]
-          else
-          investor_matrix=InvestorMatrix.where(event_id: @params[:event_id],investor_type: @params[:judge_type])
-          if investor_matrix.present?
-            @params[:current_amount]=investor_matrix.first.judge_acc_amount
-          end
-            
-            judge = ::Judge.create(
-              member_id:  member_id,  
-              event_id: @params[:event_id],
-              active:  @params[:active],
-              judge_type: @params[:judge_type],
-              current_amount:  @params[:current_amount]
-            ) 
-            if judge.save
-              judges << judge
-             
-            else
-              errors << judge.errors.full_messages
-            end
-          end
-        end
-        { judges: judges, errors: errors }
-          
-      end
-      def check_judge(member_id)
-        member = Member.includes(:users).find_by(id: member_id, active: true)
-        if member.nil? 
-          return { errors: ["Member does not exist in the database."] }
-        end
-        existing_judge = ::Judge.find_by(member_id: member_id, event_id: @params[:event_id], active: true)
-        if !existing_judge.nil?
-          if !member.users.first.nil?
-             return { errors: ["This judge #{member.users.first.email} already part of the this Event."] }
-          else
-            return { errors: ["This judge already part of the this Event."] }
-          end
-        end
-        {}
-      end
+    @params[:member_ids].each do |member_id|
+      result = check_judge(member_id)
 
-    
+      if result[:errors].present?
+        errors.concat(result[:errors])
+      else
+        judge = create_judge(member_id)
+        if judge.persisted?
+          judges << judge
+        else
+          errors.concat(judge.errors.full_messages)
+        end
+      end
     end
 
-  
+    { judges: judges, errors: errors }
+  end
+
+  private
+
+  def check_judge(member_id)
+    member = Member.includes(:users).find_by(id: member_id, active: true)
+
+    if member.nil?
+      return { errors: ["Member does not exist in the database."] }
+    end
+
+    existing_judge = ::Judge.find_by(member_id: member_id, event_id: @params[:event_id], active: true)
+    if existing_judge.present?
+      user_email = member.users.first&.email
+      return { errors: ["This judge #{user_email || 'already part of the this Event.'}"] }
+    end
+
+    existing_team_mamber = ::TeamMember.find_by(member_id: member_id, event_id: @params[:event_id], active: true)
+    if existing_team_mamber.present?
+      user_email = member.users.first&.email
+      return { errors: ["This user #{user_email || 'should not be judge because this user is team member in this event'}"] }
+    end
+
+    {}
+  end
+
+  def create_judge(member_id)
+    investor_matrix = InvestorMatrix.find_by(event_id: @params[:event_id], investor_type: @params[:judge_type])
+    @params[:current_amount] = investor_matrix&.judge_acc_amount if investor_matrix.present?
+
+    ::Judge.create(
+      member_id: member_id,
+      event_id: @params[:event_id],
+      active: @params[:active],
+      judge_type: @params[:judge_type],
+      current_amount: @params[:current_amount]
+    )
+  end
+end
