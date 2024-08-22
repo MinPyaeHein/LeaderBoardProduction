e6fd4dd12c4e2b5542d3dd786bb6f43f93e8ab58
Min Pyae Hein <minpyaehein536h@prestigein.com>
2024-08-22 13:19:45 +0700

    fix update member info

--- app/services/member/update_service.rb
+++ app/services/member/update_service.rb
@@ -1,32 +1,21 @@
-# app/services/member_service.rb
+class Member::UpdateService
+  def initialize(params)
+    @params = params
+  end
 
+  def update
+    @member = ::Member.find(@params[:member_id])
 
-    class Member::UpdateService
-      def initialize(params)
-        @params = params
-      end 
-    
-      def update
-        @member = ::Member.find(@params[:member_id]) # Assuming you're updating an existing member
-        @member.assign_attributes(
-          name: @params[:name],
-          phone: @params[:phone],
-          address: @params[:address], 
-          desc: @params[:desc],
-          faculty_id: @params[:faculty_id],
-          org_name: @params[:org_name],
-          profile_url: @params[:profile_url],
-          active: true
-        )
-      
-        if @member.save
-          { member: @member.reload } # Reload to get the latest data
-        else
-          { errors: @member.errors.full_messages }
-        end
-      end
+    # Filter out nil values from the params
+    filtered_params = @params.reject { |_, value| value.nil? }
 
-   
+    # Assign attributes that are present
+    @member.assign_attributes(filtered_params)
+
+    if @member.save
+      { member: @member.reload } # Reload to get the latest data
+    else
+      { errors: @member.errors.full_messages }
     end
-
-  
+  end
+end
