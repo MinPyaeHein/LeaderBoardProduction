62727ece2199ca93ee914066344855c73624b7e5
Min Pyae Hein <minpyaehein536h@prestigein.com>
2024-08-22 21:40:15 +0700

    remove member_id in policy

--- app/policies/tran_score_policy.rb
+++ app/policies/tran_score_policy.rb
@@ -1,6 +1,6 @@
 class TranScorePolicy < ApplicationPolicy
   def create?
-    user.member.judges.any? { |judge| judge.event_id == record.event_id } && user.member.id==record.judge_id
+    user.member.judges.any? { |judge| judge.event_id == record.event_id }
   end
 
 end
